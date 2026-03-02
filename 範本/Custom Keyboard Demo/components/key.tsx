2981368b991881b0a92790a3cb2abf4b2dff92b8
eric21364 <eric21364@gmail.com>
2026-03-02 10:55:08 +0800

    Add files via upload

--- 範本/Custom Keyboard Demo/components/key.tsx
+++ 範本/Custom Keyboard Demo/components/key.tsx
@@ -1,1 +1,33 @@
+import { Button } from "scripting"
 
+export function KeyView({
+  title, action, wide = false
+}: {
+  title: string
+  action: () => void
+  wide?: boolean
+}) {
+  return <Button
+    title={title}
+    action={action}
+    font={18}
+    fontWeight={"medium"}
+    padding={{
+      vertical: 10
+    }}
+    frame={{
+      minWidth: wide ? 80 : 34,
+    }}
+    background={"systemBackground"}
+    foregroundStyle={"label"}
+    clipShape={{
+      type: 'rect',
+      cornerRadius: 10
+    }}
+    shadow={{
+      color: 'rgba(0,0,0,0.33)',
+      radius: 1,
+      y: 1,
+    }}
+  />
+}
